33d016545bcdd1f13650a58c001bd34dd43b07ef
Razgrizsx <razgrisss41@gmail.com>
2023-05-28 16:50:08 -0300

    random movie call billboard

--- src/app/api/random/route.js
+++ src/app/api/random/route.js
@@ -2,12 +2,17 @@ import { NextResponse } from 'next/server'
 import prismadb from '../../lib/prismadb'
 
 
-export async function GET(){
+export async function POST(){
     try {
         const movieCount = await prismadb.movie.count()
-         const index = Math.floor(Math.random() * movieCount)
-        const movie = await prismadb.movie.findMany()
-        return NextResponse.json(movie)
+        const index = Math.floor(Math.random() * movieCount)
+       const movie = await prismadb.movie.findMany(
+            {
+                take: 1,
+                skip: index
+            }
+         )
+        return NextResponse.json(movie[0])
     } catch (error) {
         return NextResponse.json({'Error:': error})
     }
---
+++ src/app/components/BillBoard.jsx
@@ -0,0 +1,7 @@
+export default function BillBoard(){
+    return (
+        <div>
+            
+        </div>
+    )
+}
