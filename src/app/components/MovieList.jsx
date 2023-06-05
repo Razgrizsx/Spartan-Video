abe35e3eda73da8af87b7c2f7474315bfac2747d
Christian Rizzo <razgrisss41@gmail.com>
2023-06-05 20:36:06 -0300

    Mark II

--- src/app/components/MovieList.jsx
+++ src/app/components/MovieList.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import MovieCard from "./MovieCard";
 
 export default async function MovieList({title}){
-    
-    const {data} = await axios.get(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/movies`)
+    let data = []
+    data = await axios.get(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/movies`)
     
     return (
         <div className="px-4 md:px-12 mt-4 space-y-8 h-[300px]">
@@ -13,13 +13,12 @@ export default async function MovieList({title}){
                 {title}
             </p>
             <div className="grid grid-cols-4 gap-2">
-            {data ? data?.map((movie) => { 
+            {data?.data.map((movie) => { 
                 return(
                 <MovieCard key={movie.title} movie={movie} />
             )
             } 
-            ) : <div>Loading...</div>}
-            </div>
+            ) 
         </div>
         </div>
     )
