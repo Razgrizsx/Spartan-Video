cd0499736ce26aa27d028ac92396f3fa52807a63
Christian Rizzo <razgrisss41@gmail.com>
2023-06-06 15:33:45 -0300

    Update route.js

--- src/app/api/favorites/route.js
+++ src/app/api/favorites/route.js
@@ -2,6 +2,7 @@ import prismadb from '../../lib/prismadb'
 import { getServerSession } from 'next-auth/next'
 import {authOptions} from '../../lib/auth'
 import { NextResponse } from 'next/server'
+export const dynamic = 'force-dynamic'
 
 export async function GET(request) {
   try {
