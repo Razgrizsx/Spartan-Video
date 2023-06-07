65e3b53cfde8897913b86980968e6f90d9e7cca2
Christian Rizzo <razgrisss41@gmail.com>
2023-06-07 20:28:38 -0300

    Update MovieFullCard.jsx

--- src/app/components/MovieFullCard.jsx
+++ src/app/components/MovieFullCard.jsx
@@ -7,12 +7,9 @@ export default function MovieCard({movie}){
  
     return (
         <div className="group bg-zinc-900 col-span relative h-[12vw]">
-            <img className="cursor-pointer object-cover transition duration shadow-xl rounded-md delay-300 w-full h-[12vw]" src={movie.thumbnailurl} alt="thumbnail" />
-            <div className="opacity-90 absolute top-0 transition duration-200 z-10 sm:visible delay-300 w-full scale-0 ">
+            <img className="cursor-pointer object-cover transition duration shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 delay-300 w-full h-[12vw]" src={movie.thumbnailurl} alt="thumbnail" />
+            <div className="opacity-0 absolute top-0 transition duration-200 z-10 invisible sm:visible delay-300 w-full scale-0 group-hover:scale-110 group-hover:-translate-y-[6vw] group-hover:translate-x-[2vw] group-hover:opacity-100">
                 <img className="cursor-pointer object-cover transition duration shadow-xl rounded-t-md w-full h-[12vw]" src={movie.thumbnailurl} alt="thumbnail" />
-            
-            
-            </div>
             <div className="z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md">
                 <div className="flex flesx-row items-center gap-3">
                     <div className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-neutral-500 rounded-full flex justify-center items-center transition hover:bg-neutral-600" 
@@ -43,6 +40,7 @@ export default function MovieCard({movie}){
                 <p className='text-white text-[10px] lg:text-sm'>
                     {movie.genre}
                 </p>
+                </div>
             </div>
             </div>
         </div>
